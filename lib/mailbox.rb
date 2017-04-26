a16de430102461b1bfd80235a35c40d6d71f7521
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-26 09:09:44 +0800

    homework done

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,13 +1,25 @@
 class MailBox
+
+  attr_accessor :letters, :packages
+
   def initialize(letters, packages)
+    @letters = letters
+    @packages = packages
   end
 
   def add_letter
+    @letters += 1
+    self
   end
 
   def add_package
+    @packages += 1
+    self
   end
 
   def clear
+    @packages = 0
+    @letters = 0
+    self
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,17 @@
 class Tree
+
+  attr_accessor :height, :age
+  attr_reader :type
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
+
   end
 
   def grow_tree(water_amount)
+    @height += water_amount * 0.1
+    self
   end
 end
